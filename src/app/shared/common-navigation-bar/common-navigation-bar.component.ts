5475ff9ac1e3012f0d955ec1eb677668c1483beb
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-02-19 15:20:56 +0000

    feat(fiore): fit to own needs

--- src/app/shared/common-navigation-bar/common-navigation-bar.component.ts
+++ src/app/shared/common-navigation-bar/common-navigation-bar.component.ts
@@ -1,21 +1,22 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-common-navigation-bar',
-  imports: [NgFor],
+  imports: [
+    NgIf,
+    NgFor
+  ],
   templateUrl: './common-navigation-bar.component.html',
   styleUrl: './common-navigation-bar.component.css'
 })
 export class CommonNavigationBarComponent {
+  isMobileMenuOpen = false;
+
   routeItems = [
     {
-      name: "Dashboard",
+      name: "Home",
       isActive: true
     },
-    {
-      name: "Team",
-      isActive: false
-    }
-  ]
+  ];
 }
