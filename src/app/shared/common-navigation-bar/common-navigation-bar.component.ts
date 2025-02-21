f444c19b89f220fc1005ced5079155bb79c66aa1
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-02-21 09:39:28 +0000

    feat(fiore): add dummy routing

--- src/app/shared/common-navigation-bar/common-navigation-bar.component.ts
+++ src/app/shared/common-navigation-bar/common-navigation-bar.component.ts
@@ -1,11 +1,13 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-common-navigation-bar',
   imports: [
     NgIf,
-    NgFor
+    NgFor,
+    RouterLink
   ],
   templateUrl: './common-navigation-bar.component.html',
   styleUrl: './common-navigation-bar.component.css'
@@ -16,6 +18,7 @@ export class CommonNavigationBarComponent {
   routeItems = [
     {
       name: "Home",
+      route: "/home",
       isActive: true
     },
   ];
