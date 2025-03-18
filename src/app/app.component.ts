f57dce7f004570d019e2c968ae9c8c3253979b12
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-03-18 09:37:05 +0000

    add (fiore): add spanish and header format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonNavigationBarComponent } from './shared/common-navigation-bar/common-navigation-bar.component';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,9 @@ import { CommonNavigationBarComponent } from './shared/common-navigation-bar/com
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myapp';
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en', 'es']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
