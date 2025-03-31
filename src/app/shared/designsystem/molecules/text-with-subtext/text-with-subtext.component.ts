072a5b8709767bb34b6917735b4a3ea55cba9e72
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-03-31 17:59:50 +0000

    fix(fiore): fix drawer behaviour on resize

--- src/app/shared/designsystem/molecules/text-with-subtext/text-with-subtext.component.ts
+++ src/app/shared/designsystem/molecules/text-with-subtext/text-with-subtext.component.ts
@@ -1,18 +1,19 @@
-import { NgIf } from '@angular/common';
+import { NgIf, UpperCasePipe } from '@angular/common';
 import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-text-with-subtext',
-  imports: [NgIf],
+  imports: [NgIf, UpperCasePipe],
   templateUrl: './text-with-subtext.component.html',
   styles: ``
 })
 export class TextWithSubtextComponent implements OnChanges {
   @Input() isLoading = true;
+
   @Input() header: string | null = null;
-  @Input() subtext: string | null = null;
+  @Input() subtext: string | null | undefined = null;
 
   ngOnChanges() {
-    this.isLoading = !this.header || !this.subtext;
+    this.isLoading = !this.header;
   }
 }
