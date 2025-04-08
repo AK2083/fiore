24d0b16ccf759ea1b4860b2859abc6059bbc4daa
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-04-08 07:53:27 +0000

    feat(fiore): add theme switch

---
+++ src/app/shared/designsystem/icons/sun/sun.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sun',
+  imports: [],
+  templateUrl: './sun.component.html',
+  styles: ``
+})
+export class SunComponent {
+
+}
