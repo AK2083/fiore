06bfc36b7ad9db50b53d2339a033d1a3cff96b57
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-04-21 07:02:28 +0000

    add(fiore): add translation

--- src/app/pages/auth-user/auth-user.component.ts
+++ src/app/pages/auth-user/auth-user.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from '../../shared/designsystem/atoms/header/header.component';
 import { SimplePanelComponent } from '../../shared/designsystem/atoms/simple-panel/simple-panel.component';
 import { NgClass, NgIf } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-auth-user',
-  imports: [NgIf, NgClass, HeaderComponent, SimplePanelComponent],
+  imports: [NgIf, NgClass, TranslatePipe, HeaderComponent, SimplePanelComponent],
   templateUrl: './auth-user.component.html',
   styles: ``
 })
