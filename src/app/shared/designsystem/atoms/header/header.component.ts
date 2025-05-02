f231da849b324870abd0beace6d709f59761bf22
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-05-02 19:10:51 +0000

    refactor(fiore): add header text size

--- src/app/shared/designsystem/atoms/header/header.component.ts
+++ src/app/shared/designsystem/atoms/header/header.component.ts
@@ -1,13 +1,15 @@
 import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { TextSizeDirective } from '../../directives/text-size.directive';
 
 @Component({
   selector: 'app-header',
-  imports: [NgIf],
+  imports: [NgIf, TextSizeDirective],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+  @Input() textSize: 'small' | 'medium' | 'large' = 'medium';
   @Input() title: string | null = null;
   @Input() isLoading = true;
 }
