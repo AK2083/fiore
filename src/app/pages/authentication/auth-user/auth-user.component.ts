0200dc339becb925414f8b71554c2078f36ffe2c
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-05-04 15:23:31 +0000

    add(fiore): add credential hints

--- src/app/pages/authentication/auth-user/auth-user.component.ts
+++ src/app/pages/authentication/auth-user/auth-user.component.ts
@@ -1,17 +1,40 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../shared/designsystem/atoms/header/header.component';
 import { SimplePanelComponent } from '../../../shared/designsystem/atoms/simple-panel/simple-panel.component';
 import { NgClass, NgIf } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-user',
-  imports: [NgIf, NgClass, TranslatePipe, HeaderComponent, SimplePanelComponent],
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+    NgClass,
+    TranslatePipe,
+    HeaderComponent,
+    SimplePanelComponent,
+  ],
   templateUrl: './auth-user.component.html',
-  styles: ``
+  styles: ``,
 })
 export class AuthUserComponent {
   isPasswordVisible = false;
   selectedEmailField = false;
   selectedPasswordField = false;
+
+  myForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log('Formular ist gültig!', this.myForm.value);
+      // Hier kannst du die Formulardaten verarbeiten (z.B. an einen Service senden)
+    } else {
+      console.log('Formular ist ungültig!');
+      // Hier kannst du Fehlermeldungen anzeigen oder dem Benutzer Feedback geben
+    }
+  }
 }
