e9591a48570569218b24e7f72cec64d32f5fc652
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-05-11 05:50:06 +0000

    add(fiore): add header subtitle

---
+++ src/app/shared/designsystem/organism/registration/registration.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../atoms/header/header.component';
+import { SimplePanelComponent } from '../../atoms/simple-panel/simple-panel.component';
+import { LabeledInputComponent } from '../../molecules/labeled-input/labeled-input.component';
+import { LettercaseComponent } from '../../icons/lettercase/lettercase.component';
+import { LockComponent } from '../../icons/lock/lock.component';
+import { EyeCloseComponent } from '../../icons/eye-close/eye-close.component';
+import { EyeOpenComponent } from '../../icons/eye-open/eye-open.component';
+import { TranslatePipe } from '@ngx-translate/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-registration',
+  imports: [
+    NgIf,
+    ReactiveFormsModule,
+    TranslatePipe,
+    HeaderComponent,
+    SimplePanelComponent,
+    LabeledInputComponent,
+    LettercaseComponent,
+    LockComponent,
+    EyeCloseComponent,
+    EyeOpenComponent,
+  ],
+  templateUrl: './registration.component.html',
+  styles: ``,
+})
+export class RegistrationComponent {
+  isPasswordVisible = false;
+  selectedEmailField = false;
+  selectedPasswordField = false;
+
+  registrationForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Formular ist gültig!', this.registrationForm.value);
+    } else {
+      console.log('Formular ist ungültig!');
+    }
+  }
+
+  isEMailFocused(isFocused: boolean) {
+    this.selectedEmailField = isFocused;
+  }
+
+  isPasswordFocused(isFocused: boolean) {
+    this.selectedPasswordField = isFocused;
+  }
+
+  get emailControl(): FormControl {
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get pwdControl(): FormControl {
+    return this.registrationForm.get('password') as FormControl;
+  }
+}
