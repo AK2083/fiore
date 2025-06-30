c9f43ab098494a9237575ba512e8a09dc96de681
Adrian Helmchen <adrian.helmchen@outlook.de>
2025-06-30 12:25:46 +0000

    add(fiore): add language service log

--- src/app/core/services/localize/translation.wrapper.service.ts
+++ src/app/core/services/localize/translation.wrapper.service.ts
@@ -1,34 +1,43 @@
-import { inject, Injectable, Injector, runInInjectionContext, Signal } from '@angular/core';
+import {
+  inject,
+  Injectable,
+  Injector,
+  runInInjectionContext,
+  Signal,
+} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { scopedLoggerFactory } from '@core/helper/logging/scope.logger.factory';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TranslationWrapperService {
   private translate = inject(TranslateService);
+  private loggerService = scopedLoggerFactory(TranslationWrapperService);
   private readonly injector = inject(Injector);
-  
+
   setLanguage(lang: string) {
     this.translate.use(lang);
   }
 
   getCurrentLanguage(): string {
+    this.loggerService.log('Get language', this.translate.currentLang);
     return this.translate.currentLang;
   }
 
   getAvailableLanguages(): string[] {
+    this.loggerService.log('Get languages', this.translate.getLangs());
     return this.translate.getLangs();
   }
 
   getDefaultLanguage(): string {
+    this.loggerService.log('Get default lang', this.translate.defaultLang);
     return this.translate.defaultLang;
   }
 
   t(key: string, params?: Record<string, number | string>): Signal<string> {
     return runInInjectionContext(this.injector, () => {
-      // Stelle sicher, dass der initialValue korrekt geholt wird,
-      // auch wenn der Observable noch nicht emittiert hat.
       return toSignal(this.translate.stream(key, params), {
         initialValue: this.translate.instant(key, params),
       });
